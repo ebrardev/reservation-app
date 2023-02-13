3be3442775977b1c5893c2d4667a73c170879e1d
Ebrar <16155052@mersin.edu.tr>
2023-02-13 17:07:47 +0300

    featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import "./home.css"
@@ -8,7 +9,8 @@ const Home = () => {
         <Navbar/>
         <Header/>
         <div className="homeContainer">
-          
+          <Featured/>
+          <h1 className="homeTitle">Browse by property type</h1>
         </div>
     </div>
   )
